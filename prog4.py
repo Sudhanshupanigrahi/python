c54de21182d31e3b3afc3afd3ac84c8cfefdcf09
Sudhanshupanigrahi <sudhanshupanigrahi80@gmail.com>
2025-08-19 00:21:33 +0530

    prog4.py:sumandmultiplication

---
+++ prog4.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+s=a+b
+print("sum of first and second number:",s)
+m=b*c
+print("Multiplication of second and third number:",m)

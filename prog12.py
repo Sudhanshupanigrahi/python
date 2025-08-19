74d86248a57f1370294c2ba72f6a8f08fd909b58
Sudhanshupanigrahi <sudhanshupanigrahi80@gmail.com>
2025-08-19 14:23:35 +0530

    prog12.py:vote

---
+++ prog12.py
@@ -0,0 +1,7 @@
+a=int(input("The person Age="))
+if a>18:
+    print("This person is eligible for vote")
+if a<18:
+    print("This person is not eligible for vote")
+if a==18:
+    print("This person is eligible for vote")        

1f742b2abe92bbdabfe47badf49fb54d89448738
Sudhanshupanigrahi <sudhanshupanigrahi80@gmail.com>
2025-08-19 13:47:57 +0530

    prog5.py:tp

---
+++ prog5.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+t=a+b+c
+print("The total mark is=",+)
+p=t/300*100
+print("The percentage of mark is=",p)
